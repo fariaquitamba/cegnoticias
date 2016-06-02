ece1eb238f83e4c9de9af1a3e7437080e48883c7
Faria Quitamba <fariaquitamba5@gmail.com>
2016-06-02 17:34:00 +0100

    Projecto de aula, portal de noticias

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/default.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css/custom.css" media="screen" title="no title" charset="utf-8">
+    <title>Ceg Noticias</title>
+  </head>
+  <body>
+    <!-- inicio do cabeçalho -->
+    <header class="border-1 fullwidth c-red fixed" style="height: 5em">
+
+    </header>
+
+    <!-- seccao principal dos conteudos -->
+    <div class="fullwidth border-1" style="height: 100em;">
+      <section class="fullwidth border-1" style="height: 30em">
+        <h1>slider</h1>
+      </section>
+      <!-- seccao para todos os posts do jornal -->
+      <section class="container border-1 fullwidth" style="height: 30em">
+        <aside class="">
+
+        </aside>
+        <aside class="">
+
+        </aside>
+      </section>
+    </div>
+  </body>
+</html>
