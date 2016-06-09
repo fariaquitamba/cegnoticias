0543f0fd8d5a43ac5b8014483c3ad4703dbb3109
Faria Quitamba <fariaquitamba5@gmail.com>
2016-06-09 17:16:13 +0100

    Concluímos o layout, começamos a trabalhar no header

--- index.php
+++ index.php
@@ -4,28 +4,39 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/default.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="css/custom.css" media="screen" title="no title" charset="utf-8">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Ceg Noticias</title>
   </head>
   <body>
     <!-- inicio do cabeçalho -->
-    <header class="border-1 fullwidth c-red fixed" style="height: 5em">
+    <header class="fullwidth c-red fixed" style="height: 5em">
+      <h1 class="left text-white padding-top">CEG NOTICIAS</h1>
+      <nav class="right padding-top-2">
+        <a href="#" class="text-white padding"><i class="material-icons">home</i></a>
+        <a href="#" class="text-white padding">ENTRETENIMENTO</a>
+        <a href="#" class="text-white padding">DESPORTO</a>
+        <a href="#" class="text-white padding ">TECNOLOGIA</a>
+        <a href="#" class="text-white padding ">COMUNIDADE</a>
+          <a href="#" class="text-white padding "><i class="material-icons">search</i></a>
+      </nav>
 
     </header>
 
     <!-- seccao principal dos conteudos -->
-    <div class="fullwidth border-1" style="height: 100em;">
+    <div class="fullwidth border-1" style="height: auto;">
       <section class="fullwidth border-1" style="height: 30em">
         <h1>slider</h1>
       </section>
       <!-- seccao para todos os posts do jornal -->
       <section class="container border-1 fullwidth" style="height: 30em">
-        <aside class="">
+        <aside class="border-1 col-l6 left"  style="height: 30em">
 
         </aside>
-        <aside class="">
+        <aside class="border-1 col-third left" style="height: 30em">
 
         </aside>
       </section>
+      <footer class="container border-1 fullwidth" style="height: 10em"></footer>
     </div>
   </body>
 </html>
