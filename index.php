6feeffdea195765ab15305550fee7f74342f743c
Faria Quitamba <fariaquitamba5@gmail.com>
2016-06-21 14:33:04 +0100

    Inicio do Cabeçalho

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <link rel="stylesheet" href="css/default.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="css/custom.css" media="screen" title="no title" charset="utf-8">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <title>Ceg Noticias</title>
   </head>
   <body>
     <!-- inicio do cabeçalho -->
-    <header class="fullwidth c-red fixed" style="height: 5em">
+    <header id="cabecalho">
+
+    </header>
+    <header class="fullwidth c-red fixed card" style="height: 5em">
       <h1 class="left text-white padding-top">CEG NOTICIAS</h1>
       <nav class="right padding-top-2">
         <a href="#" class="text-white padding"><i class="material-icons">home</i></a>
@@ -17,7 +21,7 @@
         <a href="#" class="text-white padding">DESPORTO</a>
         <a href="#" class="text-white padding ">TECNOLOGIA</a>
         <a href="#" class="text-white padding ">COMUNIDADE</a>
-          <a href="#" class="text-white padding "><i class="material-icons">search</i></a>
+        <a href="#" class="text-white padding "><i class="material-icons">search</i></a>
       </nav>
 
     </header>
@@ -30,13 +34,29 @@
       <!-- seccao para todos os posts do jornal -->
       <section class="container border-1 fullwidth" style="height: 30em">
         <aside class="border-1 col-l6 left"  style="height: 30em">
+          <div class="">
 
+          </div>
         </aside>
         <aside class="border-1 col-third left" style="height: 30em">
 
         </aside>
       </section>
-      <footer class="container border-1 fullwidth" style="height: 10em"></footer>
+      <footer class="container padding-2 c-dark-red fullwidth" style="height: 10em">
+        <div class="col-third ">
+          <h1>Newsletter</h1>
+        </div>
+        <div class="col-third ">
+          <h1>&copy; 2016</h1>
+        </div>
+        <div class="col-third ">
+          <ul class="c-list">
+            <li class=""><a href="#" class="text-white margin-top d-block">Políticas e Termos</a></li>
+            <li class="margin-left margin-right"><a href="#" class="text-white margin-top d-block"> Recrutamento</a></li>
+            <li><a href="#" class="text-white margin-top d-block">Anúncie aqui</a></li>
+          </ul>
+        </div>
+      </footer>
     </div>
   </body>
 </html>
